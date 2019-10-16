014bbfc5ae79c562609bf51533229f36558dc8db
Ivan Zelenkov <vertvery@gmail.com>
2019-10-17 01:05:49 +0300

    initial commit

---
+++ src/test/java/com/microservices/choreographyorchestrator/ChoreographyOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.choreographyorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreographyOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
