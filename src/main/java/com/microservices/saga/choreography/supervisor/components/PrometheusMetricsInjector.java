4650c4ae1740a215415ebe3538f70dd4a01e9355
Alex Strive <alexstrive@pm.me>
2020-06-24 15:04:12 +0300

    add documentation for metrics injector

--- src/main/java/com/microservices/saga/choreography/supervisor/components/PrometheusMetricsInjector.java
+++ src/main/java/com/microservices/saga/choreography/supervisor/components/PrometheusMetricsInjector.java
@@ -6,12 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import io.prometheus.client.exporter.MetricsServlet;
 
+/**
+ * We have Micrometer as main metrics generator which exposed its results at using actuator
+ * Since its needed to make alternative metrics generation we use official Prometheus Java Client for it
+ * Here we inject their servlet into our HTTP server at specific url
+ * This servlet generates content in prometheus format by accessing specified url
+ *
+ * More:
+ * https://github.com/prometheus/client_java#http
+ */
 @Component
 public class PrometheusMetricsInjector {
     @Bean
     public ServletRegistrationBean injectPrometheusServlet() {
+
+        // URL at which we expose alternative generated metrics
         var targetUrl = "/metrics-prometheus";
+
+        // Create servlet bound to global metrics registry
         var prometheusServlet = new MetricsServlet(CollectorRegistry.defaultRegistry);
+
+        // Inject previously created servlet into HTTP server
         return new ServletRegistrationBean(prometheusServlet, targetUrl);
     }
 }
